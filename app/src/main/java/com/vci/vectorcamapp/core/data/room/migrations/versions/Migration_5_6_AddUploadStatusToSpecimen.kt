1c7bf31ca735df5a18a989ab3bca76cec9749661
ThomasT0830 <91466914+ThomasT0830@users.noreply.github.com>
2025-07-06 18:23:27 -0400

    SOF-167: Fixed formatting of migration file

--- app/src/main/java/com/vci/vectorcamapp/core/data/room/migrations/versions/Migration_5_6_AddUploadStatusToSpecimen.kt
+++ app/src/main/java/com/vci/vectorcamapp/core/data/room/migrations/versions/Migration_5_6_AddUploadStatusToSpecimen.kt
@@ -6,7 +6,7 @@ import androidx.sqlite.db.SupportSQLiteDatabase
 val MIGRATION_5_6_ADD_UPLOAD_STATUS_TO_SPECIMEN = object : Migration(5, 6) {
     override fun migrate(db: SupportSQLiteDatabase) {
         db.execSQL(
-            "ALTER TABLE specimen ADD COLUMN uploadStatus TEXT NOT NULL DEFAULT 'NOT_STARTED'"
+            "ALTER TABLE `specimen` ADD COLUMN `uploadStatus` TEXT NOT NULL DEFAULT 'NOT_STARTED'"
         )
     }
 }
